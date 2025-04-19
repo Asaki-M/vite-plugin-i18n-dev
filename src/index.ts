59bf8a9fc16a02b43951f13fa971da6c03be36e8
qinghuan <1041220793@qq.com>
2025-04-20 00:07:49 +0800

    fix: ts check bug

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import type { Plugin } from 'vite'
+import sirv from 'sirv'
+import { DIR_CLIENT } from './dir'
 
-
-function VitepluginEasy(): Plugin {
+function VitepluginI18nDev(): Plugin {
 
   return {
-    name: 'vite-plugin-easy',
+    name: 'vite-plugin-i18n-dev',
+    apply: 'serve',
+    configureServer(server) {
+      const base = (server.config.base) || '/'
+      server.middlewares.use(`${base}__i18n__dev__`, sirv(DIR_CLIENT, {
+        single: true,
+        dev: true,
+      }))
+    }
   }
 }
 
-export default VitepluginEasy
+export default VitepluginI18nDev
